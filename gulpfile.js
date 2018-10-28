0b90b4f253b48661fe4bd4d6cf111ed7e996e11e
andrewSaver <andysaver@gmail.com>
2018-10-27 20:53:38 -0400

    change app to docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('app/scss/main.scss')
+  return gulp.src('docs/scss/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('docs/css'))
 });
